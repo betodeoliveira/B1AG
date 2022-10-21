f93082519af61c46da62d10965b22be0957c9faf
Alberto de Olivira <betodeoliveira@outlook.com>
2022-10-21 14:39:37 -0300

    Create disable-page-scroll.js

---
+++ disable-page-scroll.js
@@ -0,0 +1,40 @@
+let $body = $(document.body);
+    let scrollPosition = 0;
+
+    // Disables page scroll
+    $('.is-mobile-menu, .set-contact-service-empty, .services_install-service').on('click', function () {
+        var oldWidth = $body.innerWidth();
+        scrollPosition = window.pageYOffset;
+        $body.css('overflow', 'hidden');
+        $body.css('position', 'relative');
+        // $body.css('top', `-${scrollPosition}px`);
+        $body.width(oldWidth);
+    });
+
+    // Enables page scroll
+    $('.menu_close-btn, .contact_background, .contact_close-btn').on('click', function () {
+        if ($body.css('overflow') != 'hidden') { scrollPosition = window.pageYOffset; }
+        $body.css('overflow', '');
+        $body.css('position', '');
+        // $body.css('top', '');
+        $body.width('');
+        // $(window).scrollTop(scrollPosition);
+    });
+
+    // Toggles page scroll
+    $('[scroll="both"]').on('click', function () {
+        if ($body.css('overflow') !== 'hidden') {
+            var oldWidth = $body.innerWidth();
+            scrollPosition = window.pageYOffset;
+            $body.css('overflow', 'hidden');
+            $body.css('position', 'relative');
+            // $body.css('top', `-${scrollPosition}px`);
+            $body.width(oldWidth);
+        } else {
+            $body.css('overflow', '');
+            $body.css('position', '');
+            // $body.css('top', '');
+            $body.width('');
+            // $(window).scrollTop(scrollPosition);
+        }
+    });
